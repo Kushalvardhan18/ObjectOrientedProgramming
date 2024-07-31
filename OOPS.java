9f481499a4136e36074cd27a6fa5137a4a37f609
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-31 12:01:32 +0530

    Update OOPS.java

--- OOPS.java
+++ OOPS.java
@@ -1,2 +1,33 @@
 public class OOPS {
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen();  // Constructor -- created a pen object
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    // pro and func
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Studnt{
+    String name;
+    int age;
+    float percentage;
+
+
+    void calcPercentage (int phy,int chem,int math){
+        percentage =(phy+chem+math)/3;
+    }
 }
