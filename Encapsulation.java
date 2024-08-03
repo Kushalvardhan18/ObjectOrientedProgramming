0168b03ac7c5f4eba0f4ecb438237761ee113c8d
Kushal Vardhan <vardhank140102@gmail.com>
2024-08-03 10:57:15 +0530

    encapsulation

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,5 +1,29 @@
-public class Encapsulation {
+public class Encapsulation {  
+    // Wrapping up of Data and Methods under a Single unit. Also implements data hiding.
     public static void main(String[] args){
-        
+        // person object created
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
+ 
+        // Using methods to get the values from the
+        // variables
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
     }
+}
+class Person {
+    // Encapsulating the name and age
+    // only approachable and used using
+    // methods defined
+    private String name;
+    private int age;
+ 
+    public String getName() { return name; }
+ 
+    public void setName(String name) { this.name = name; }
+ 
+    public int getAge() { return age; }
+ 
+    public void setAge(int age) { this.age = age; }
 }
